096edd335b7e626c80db4b87e647d4c690fbc4d9
Ustinch <88932719+Ustinch@users.noreply.github.com>
2026-09-25 18:03:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def main(request: Request):
             "Content-Type": "application/json",
         },
         json={
-            "model": "google/gemini-2.0-flash-exp:free",
+            "model": "openrouter/free",
             "messages": [{"role": "user", "content": user_text}],
         },
         timeout=4,
